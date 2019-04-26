52075716d0b683db9d55109bc24429046cc14207
Mingze Chen <mingzec@gmail.com>
2019-04-26 11:02:50 +0200

    optimize solution for task_gonna_be_500_miles

--- miles.py
+++ miles.py
@@ -3,9 +3,12 @@ def main():
     print("Kraftstoffverbrauch eines Kfz")
     print("*****************************")
     distance = int(
-        input('Bitte die gewünschte Kilometer-Entfernung eingeben:\n '))
+        input('\nBitte die gewünschte Kilometer-Entfernung eingeben:\n '))
     liter = int(input('Bitte den Liter-Betrag des Verbrauches eingeben:\n '))
-    print("Der Kraftstoffverbrauch beträgt durchschnittlich: " +
-          '{0:.15f}'.format(liter / distance * 100))
+    if distance > 0:
+        print("\nDer Kraftstoffverbrauch beträgt durchschnittlich: " +
+              '{0:.15f}'.format(liter / distance * 100))
+    else:
+        print("\nDie gewünschte Kilometer-Entfernung soll immer positiv sein!")
 if __name__ == '__main__':
     main()
