8362e5ee3657695ce13c2bec86b1e5ad0ca6ec99
Mingze Chen <mingzec@gmail.com>
2019-05-23 20:30:08 +0200

    finish wahl, correct pick and romanum

--- romanum.py
+++ romanum.py
@@ -1,29 +1,25 @@
 def romanum(zahl):
     result = 0
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if zahl.islower():
         print("Please check the validity of input, it should be in uppercase!")
     else:
-        for x in zahl:
-            if x == 'I':
-                result += 1
-            elif x == 'V':
-                result += 5
-            elif x == "X":
-                result += 10
-            elif x == "L":
-                result += 50
-            elif x == "C":
-                result += 100
-            elif x == 'D':
-                result += 500
-            elif x == 'M':
-                result += 1000
+        for x in range(0, len(zahl) - 1):
+            # print(zahl[x])
+            if d[zahl[x]] < d[zahl[x + 1]]:
+                result -= d[zahl[x]]
+            else:
+                result += d[zahl[x]]
+        result += d[zahl[len(zahl) - 1]]
+
     print("\nDeine Zahl lautet: " + str(result))
 
 
 def main():
     print("Imperium Romanum\n")
     zahl = input("Bitte eine römische Zahl eingeben: ")
+    # show attribute of string
+    # print(dir(zahl))
     romanum(zahl)
 
 
